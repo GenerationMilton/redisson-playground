f994acb04b6bc215a3b35d32051efb8cfc9dbbea
Milton Muñoz <livemilton@gmail.com>
2024-10-13 21:01:51 -0500

    first commit and redisson client to conection

---
+++ src/test/java/com/livemilton/redisson/test/config/RedissonConfig.java
@@ -0,0 +1,29 @@
+package com.livemilton.redisson.test.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.config.Config;
+
+import java.util.Objects;
+
+public class RedissonConfig {
+
+    private RedissonClient redissonClient;
+
+    //Connection to the database
+    public RedissonClient getClient(){
+        if(Objects.isNull(this.redissonClient)){
+            Config config = new Config();
+            config.useSingleServer()
+                    .setAddress("redis://127.0.0.1:6379");
+            redissonClient = Redisson.create(config);
+        }
+
+        return redissonClient;
+    }
+
+    public RedissonReactiveClient getReactiveClient(){
+        return getClient().reactive();
+    }
+}
---
+++ src/test/java/com/livemilton/redisson_playground/RedissonPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livemilton.redisson_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
